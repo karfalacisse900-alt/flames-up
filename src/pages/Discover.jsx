5320008b16ead505f6edb8409fd05ea4f5215fdb
karfala cisse <karfalacisse900@gmail.com>
2026-02-19 07:59:31 +0000

    File changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { motion, AnimatePresence } from "framer-motion";
 import DiscoverLogo from "../components/discover/DiscoverLogo";
 import StarRating from "../components/discover/StarRating";
-import FeaturedTool from "../components/discover/FeaturedTool";
+import DiscoverBillboard from "../components/discover/DiscoverBillboard";
 import NewNoteworthy from "../components/discover/NewNoteworthy";
 import DiscoverItemModal from "../components/discover/DiscoverItemModal";
 import DiscoverAIAssistant from "../components/discover/DiscoverAIAssistant";
