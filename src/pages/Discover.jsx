c69bc3362eee77bb2954057e201f6608ef80dc1a
karfala cisse <karfalacisse900@gmail.com>
2026-02-24 00:07:18 +0000

    File changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -260,9 +260,17 @@ export default function Discover() {
       {/* ===== APPS TAB ===== */}
       {contentTab === "apps" && (
         <>
+          {/* Why these apps collapsible */}
+          <div className="pt-3">
+            <WhyTheseApps />
+          </div>
+
+          {/* Smart filters */}
+          <SmartFilters active={smartFilter} onChange={f => { setSmartFilter(f); setActiveCategory("all"); }} />
+
           {/* Search + Filter */}
           {viewMode === "list" && (
-            <div className="px-5 pt-3 space-y-2">
+            <div className="px-5 pt-1 space-y-2">
               <div className="flex gap-2">
                 <div className="relative flex-1">
                   <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[#9B9B9B]" />
