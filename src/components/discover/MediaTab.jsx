300584d7de81a3feb470335a03dc38fe4c6b2b48
karfala cisse <karfalacisse900@gmail.com>
2026-02-25 05:50:56 +0000

    File changes

--- src/components/discover/MediaTab.jsx
+++ src/components/discover/MediaTab.jsx
@@ -495,6 +495,7 @@ export default function MediaTab({ user }) {
 
   const isMusicView = activeType === "music";
   const isBookView = activeType === "book";
+  const isMovieShowView = activeType === "movie" || activeType === "show";
   const activeFilterCount = [selectedGenre !== "all", selectedMood !== "all", sortBy !== "default"].filter(Boolean).length;
 
   return (
