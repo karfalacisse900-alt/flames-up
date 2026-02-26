64c61c7dcb44d4279344033f78afbf35fff41e4a
karfala cisse <karfalacisse900@gmail.com>
2026-02-26 15:19:07 +0000

    File changes

--- src/components/discover/MiniPlayerWrapper.jsx
+++ src/components/discover/MiniPlayerWrapper.jsx
@@ -3,7 +3,9 @@ import { MiniStickyPlayer } from "./AudioPreviewPlayer";
 import { useAudio } from "@/components/AudioContext";
 
 export default function MiniPlayerWrapper() {
-  const { currentTrack, setCurrentTrack } = useAudio();
+  const audioContext = useAudio();
+  if (!audioContext) return null;
   
+  const { currentTrack, setCurrentTrack } = audioContext;
   return <MiniStickyPlayer track={currentTrack} onClose={() => setCurrentTrack(null)} />;
 }
