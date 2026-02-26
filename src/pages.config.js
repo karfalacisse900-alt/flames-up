850aa47e65c9a2d675d8841913b05d9a530aa959
karfala cisse <karfalacisse900@gmail.com>
2026-02-26 13:25:30 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -50,6 +50,7 @@
 import AdminModeration from './pages/AdminModeration';
 import Art from './pages/Art';
 import ArtStudio from './pages/ArtStudio';
+import Collections from './pages/Collections';
 import Discover from './pages/Discover';
 import DiscoverForum from './pages/DiscoverForum';
 import EditServiceProfile from './pages/EditServiceProfile';
@@ -66,12 +67,11 @@ import NotificationSettings from './pages/NotificationSettings';
 import Notifications from './pages/Notifications';
 import PostComments from './pages/PostComments';
 import PostDetail from './pages/PostDetail';
+import Profile from './pages/Profile';
 import Referral from './pages/Referral';
 import Shop from './pages/Shop';
 import UserProfile from './pages/UserProfile';
 import Wallet from './pages/Wallet';
-import Collections from './pages/Collections';
-import Profile from './pages/Profile';
 import __Layout from './Layout.jsx';
 
 
@@ -79,6 +79,7 @@ export const PAGES = {
     "AdminModeration": AdminModeration,
     "Art": Art,
     "ArtStudio": ArtStudio,
+    "Collections": Collections,
     "Discover": Discover,
     "DiscoverForum": DiscoverForum,
     "EditServiceProfile": EditServiceProfile,
@@ -95,12 +96,11 @@ export const PAGES = {
     "Notifications": Notifications,
     "PostComments": PostComments,
     "PostDetail": PostDetail,
+    "Profile": Profile,
     "Referral": Referral,
     "Shop": Shop,
     "UserProfile": UserProfile,
     "Wallet": Wallet,
-    "Collections": Collections,
-    "Profile": Profile,
 }
 
 export const pagesConfig = {
