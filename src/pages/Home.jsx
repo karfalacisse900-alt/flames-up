81879f964263d68509a4356401378bb1d60c8de3
karfala cisse <karfalacisse900@gmail.com>
2026-02-28 00:00:17 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { base44 } from "@/api/base44Client";
 import WelcomePopup from "../components/home/WelcomePopup";
 import CommunityFeed from "../components/community/CommunityFeed";
 import TrendingCarousel from "../components/home/TrendingCarousel";
-import HomeHeader from "../components/home/HomeHeader";
+import HomeHeader from "@/components/home/HomeHeader";
 
 export default function Home() {
   const [user, setUser] = useState(null);
