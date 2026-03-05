d8f20119225ed7aaef8d928c3d23dcb11af2b0dc
karfala cisse <karfalacisse900@gmail.com>
2026-03-05 06:50:03 +0000

    File changes

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { ArrowLeft, Heart, Send, EyeOff, UserPlus, UserCheck } from "lucide-react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "../utils";
