190b05c13b57d7d849d044dd9ce056dda2b5577e
karfala cisse <karfalacisse900@gmail.com>
2026-03-07 15:48:57 +0000

    File changes

--- src/components/layout/RightSidebar.jsx
+++ src/components/layout/RightSidebar.jsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { base44 } from "@/api/base44Client";
-import { Link } from "react-router-dom";
-import { createPageUrl } from "@/utils";
-import { MessageSquare, Sparkles, TrendingUp } from "lucide-react";
+import React from "react";
 
 export default function RightSidebar() {
   return null;
