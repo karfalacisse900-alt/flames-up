35cbaf86545cc8cbf782a0677c95160cf8141f31
karfala cisse <karfalacisse900@gmail.com>
2026-03-07 15:25:48 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import WelcomePopup from "../components/home/WelcomePopup";
 import CommunityFeed from "../components/community/CommunityFeed";
 import HomeHeader from "@/components/home/HomeHeader";
 import DidYouKnowSection from "@/components/home/DidYouKnowSection";
+import DesktopFeed from "@/components/feed/DesktopFeed";
 
 export default function Home() {
   const [user, setUser] = useState(null);
