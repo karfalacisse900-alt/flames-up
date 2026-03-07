00bcb43d4be1927f52118af40bb8d3dc6e8420b7
karfala cisse <karfalacisse900@gmail.com>
2026-03-07 20:35:22 +0000

    File changes

--- src/components/community/CommunityPostCard.jsx
+++ src/components/community/CommunityPostCard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import { base44 } from "@/api/base44Client";
 
-import { MessageCircle, Share2, Bookmark, UserPlus, UserCheck, Trash2, MoreHorizontal, Flag, Eye, Link as LinkIcon, EyeOff, MapPin } from "lucide-react";
+import { MessageCircle, Share2, Bookmark, UserPlus, UserCheck, Trash2, MoreHorizontal, Flag, Link as LinkIcon, EyeOff, MapPin } from "lucide-react";
 import AutoplayVideo from "./AutoplayVideo";
 import PhotoCarousel from "./PhotoCarousel";
 import SavePostModal from "./SavePostModal";
