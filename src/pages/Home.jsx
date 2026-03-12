1589d6fad153f1ed9f4fb95db033d902b308660d
karfala cisse <karfalacisse900@gmail.com>
2026-03-12 19:31:19 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import HomeHeader from "@/components/home/HomeHeader";
 import DidYouKnowSection from "@/components/home/DidYouKnowSection";
 import CommunityFeed from "../components/community/CommunityFeed";
 import StatusBar from "@/components/home/StatusBar";
-import LiveActivityFeed from "@/components/feed/LiveActivityFeed";
 
 export default function Home() {
   const [user, setUser] = useState(null);
