b9dfd8c5e16bef846bb9c7199587f8754e4ccb62
karfala cisse <karfalacisse900@gmail.com>
2026-03-12 20:57:48 +0000

    File changes

--- src/components/groups/GroupCard.jsx
+++ src/components/groups/GroupCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Star, Users, DollarSign, ChevronRight, Heart } from "lucide-react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
