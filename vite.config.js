b2a1e92621b7ddd45483f8038ed559196eb8aecc
karfala cisse <karfalacisse900@gmail.com>
2026-03-12 21:32:14 +0000

    Manual code change

--- vite.config.js
+++ vite.config.js
@@ -3,39 +3,39 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-logLevel: "error",
+  logLevel: "error",
 
-server: {
-host: true,
-port: 5173,
-strictPort: false,
-allowedHosts: [".modal.host"],
-hmr: {
-protocol: "wss",
-clientPort: 443
-}
-},
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false,
+    allowedHosts: "all",
+    hmr: {
+      protocol: "wss",
+      clientPort: 443
+    }
+  },
 
-preview: {
-host: true,
-port: 5173
-},
+  preview: {
+    host: true,
+    port: 5173
+  },
 
-plugins: [
-base44({
-legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
-hmrNotifier: true,
-navigationNotifier: true,
-visualEditAgent: true
-}),
-react()
-],
+  plugins: [
+    base44({
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
+      hmrNotifier: true,
+      navigationNotifier: true,
+      visualEditAgent: true
+    }),
+    react()
+  ],
 
-optimizeDeps: {
-include: ["react", "react-dom"]
-},
+  optimizeDeps: {
+    include: ["react", "react-dom"]
+  },
 
-build: {
-target: "esnext"
-}
+  build: {
+    target: "esnext"
+  }
 });
