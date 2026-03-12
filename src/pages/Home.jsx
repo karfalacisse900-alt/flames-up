a4b6fdf83c90830ec9677a03c7154c4d05bdf454
karfala cisse <karfalacisse900@gmail.com>
2026-03-12 18:25:46 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,7 @@ export default function Home() {
       {divider}
       <CommunityFeed user={user} />
       <WelcomePopup />
+      {user && <LiveActivityFeed maxItems={8} autoRefreshMs={4000} />}
     </div>
   );
 }
