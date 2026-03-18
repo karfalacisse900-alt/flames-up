035bd258c43b398a5f48beab6c014676203281a8
karfala cisse <karfalacisse900@gmail.com>
2026-03-18 19:45:38 +0000

    File changes

--- src/components/places/AddPlaceModal.jsx
+++ src/components/places/AddPlaceModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { X, MapPin, Upload, Image as ImageIcon } from "lucide-react";
 import { base44 } from "@/api/base44Client";
 
